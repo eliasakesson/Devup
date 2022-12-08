8e8baad964fda393c73d0026539599f21a246107
Swezyn <akessonelias@gmail.com>
2022-12-08 19:03:01 +0100

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,21 +1,38 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { MdEmail } from 'react-icons/md'
 import styles from '../styles/navbar.module.css'
 
 export default function Footer() {
   return (
-    <div className={styles.footercontainer}>
-      <div className={styles.links}>
-        <Link href='/'>Hem</Link>
-        <Link href='/services'>Tjänster</Link>
-        <Link href='/portfolio'>Portfolio</Link>
-        <Link href='/about'>Om oss</Link>
-        <Link href='/contact'>Kontakt</Link>
+    <footer className={styles.footercontainer}>
+      <div className={styles.footer}>
+        <div className={styles.footerinfo}>
+          <Link style={{textDecoration: "none"}} href="/"><div className={styles.logo}><Image src='/logo.png' alt='' width={1000} height={1000} /><h2>DevUp</h2></div></Link>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dapibus nibh et urna pharetra iaculis. Curabitur porta nisl est, eu hendrerit urna pulvinar sed. Nullam arcu mi, aliquet et sem id, laoreet efficitur justo.</p>
+        </div>
+        <div className={styles.links}>
+          <h3>Våra Tjänster</h3>
+          <Link href='/services#web' className='link'>Webbutveckling</Link>
+          <Link href='/services#app' className='link'>Webb Appar</Link>
+          <Link href='/services#mobile' className='link'>Mobil Appar</Link>
+        </div>
+        <div className={styles.contact}>
+          <h3>Kontakta oss</h3>
+          <div className={styles.email}>
+            <div className={styles.icon}>
+                <MdEmail size={20} color="#ccc" />
+            </div>
+            <div>
+              <h4>Email:</h4>
+              <a href='mailto:devupwebagency@gmail.com' className='link' >devupwebagency@gmail.com</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={styles.footerlogo}>
-        <Link style={{textDecoration: "none"}} href="/"><div className={styles.logo}><Image src='/logo.png' alt='' width={1000} height={1000} /><h2>DevUp</h2></div></Link>
-      </div>
-    </div>
+      <div className={styles.separator}></div>
+      <p>Copyright © 2022 Devup AB</p>
+    </footer>
   )
 }
