feb6adc05b275fdc37b2605e073ecc2ef6a2158f
Swezyn <akessonelias@gmail.com>
2023-04-28 18:05:36 +0200

    Updated pricing page and prices

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -6,6 +6,7 @@ import { RiDoubleQuotesL } from 'react-icons/ri';
 import { VisibleControls } from '../../hooks';
 import { motion } from 'framer-motion';
 import { Helmet } from 'react-helmet-async';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -46,6 +47,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <div ref={blob} className="mouse-follow"></div>
+            <div className="animated-blob"></div>
             <h1>
                 <motion.div initial={{translateY: "100%", opacity: 0}} animate={{translateY: 0, opacity: 1}}>Upptäck den nya</motion.div>
                 <motion.div initial={{translateY: "100%", opacity: 0}} animate={{translateY: 0, opacity: 1}} transition={{delay: 0.15}}>generationen inom</motion.div>
@@ -70,7 +72,7 @@ const Help = () => {
                 visible: {opacity: 1, translateX: 0},
                 hidden: {opacity: 0, translateX: "-50%"}
             }}>Devup hjälper små & medelstora företag med sin digitala närvaro</motion.h2>
-            <div className="right">
+            <div className="bottom">
                 <motion.p animate={controls} transition={{delay: 0.5, damping: 1}} variants={{
                     visible: {opacity: 1},
                     hidden: {opacity: 0}
@@ -331,46 +333,31 @@ const Pricing = () => {
 
     return (
         <section className='pricing' id='pricing'>
-            <h2>Priser</h2>
-            <h3>Våra populäraste paket</h3>
-            <ul ref={ref} className="cards">
-                <motion.li className='card' animate={controls} transition={{delay: 0.2}} variants={{
-                    visible: {opacity: 1, scaleY: 1},
-                    hidden: {opacity: 0, scaleY: 0},
-                }}>
-                    <h4>Standard Webbdesign</h4>
-                    <ul>
-                        <li>En anpassad hemsida</li>
-                        <li>Upp till 5 ytterligare sidor</li>
-                        <li>Mer avancerade designelement, t.ex. grafisk design, responsiv design och användarupplevelse</li>
-                    </ul>
-                    <span>2500-7500</span>
-                </motion.li>
-                <motion.li className='card' animate={controls} transition={{delay: 0.4}} variants={{
-                    visible: {opacity: 1, scaleY: 1},
-                    hidden: {opacity: 0, scaleY: 0},
-                }}>
-                    <h4>Standard Webbutveckling</h4>
-                    <ul>
-                        <li>Komplex webbapplikation</li>
-                        <li>Funktioner som användarautentisering och integration med databaser</li>
-                    </ul>
-                    <span>7500-15000</span>
-                </motion.li>
-                <motion.li className='card' animate={controls} transition={{delay: 0.6}} variants={{
-                    visible: {opacity: 1, scaleY: 1},
-                    hidden: {opacity: 0, scaleY: 0},
-                }}>
-                    <h4>Standard Apputveckling</h4>
-                    <ul>
-                        <li>Komplex app</li>
-                        <li>Funktioner som användarautentisering och push-notifikationer</li>
-                    </ul>
-                    <span>7500-15000</span>
-                </motion.li>
-            </ul>
-            <p>Priserna är <span>ungefärliga</span> och varierar beroende på projektet.</p>
-            <RouterLink variant='link' to='/pricing' arrow>Se alla paket</RouterLink>
+            <div className="left">
+                <h2>Priser</h2>
+                <motion.h3 animate={controls} transition={{delay: 0.2}} variants={{
+                    visible: {opacity: 1, translateX: 0},
+                    hidden: {opacity: 0, translateX: "-100%"},
+                }}>Få en responsiv hemsida för endast 999kr</motion.h3>
+                <RouterLink variant='link' to='/pricing' arrow>Se alla paket</RouterLink>
+            </div>
+            <div className="right">
+                <Link to="/pricing">
+                    <motion.div className='package' ref={ref} animate={controls} transition={{delay: 0.4}} variants={{
+                        visible: {opacity: 1, translateY: 0},
+                        hidden: {opacity: 0, translateY: "100%"},
+                    }}>
+                        <span className='tag'>Basic</span>
+                        <span className='from'>Från</span>
+                        <span className='price'>999 kr</span>
+                        <ul>
+                            <li>3 sidor</li>
+                            <li>Responsiv Design</li>
+                            <li>Kontaktformulär</li>
+                        </ul>
+                    </motion.div>
+                </Link>
+            </div>
         </section>
     );
 }
@@ -382,7 +369,7 @@ const Contact = () => {
 
     return (
         <section ref={ref} className='contact' id='contact'>
-            <h2>Kontakta oss</h2>
+            <h2>Kontakt</h2>
             <h3>
                 <motion.div animate={controls} transition={{delay: 0.2}} variants={{
                     visible: {opacity: 1, translateY: 0},
--- src/pages/Pricing/Pricing.tsx
+++ src/pages/Pricing/Pricing.tsx
@@ -11,106 +11,93 @@ const Pricing = () => {
                 <meta name="description" content="Detta är priserna för våra tjänster. Vi erbjuder ett brett utbud av tjänster som passar alla behov. Vi erbjuder även en gratis konsultation där vi går igenom dina behov och ger dig en uppskattning på pris." />
             </Helmet>
             <div className='pricing-screen'>
-                <h1>Prissättning</h1>
-                <motion.h2 initial={{opacity: 0, translateX: "-100%"}} animate={{opacity: 1, translateX: 0}}>Våra prispaket</motion.h2>
-                <motion.p initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.2}}>Priserna nedan är ungefärliga och varierar beroende på storlek och tid som projektet tar.</motion.p>
-                <motion.p initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.4}}>För en bättre uppskattning på pris, <RouterLink to='/contact' variant='link' arrow>Kontakta Oss</RouterLink></motion.p>
-                <motion.table initial={{opacity: 0, translateY: "20%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.6}}>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Basic</th>
-                            <th>Standard</th>
-                            <th>Premium</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><h3>Webbdesign</h3></td>
-                            <td>
-                                <h4>Basic</h4>
-                                <ul>
-                                    <li>En anpassad hemsida</li>
-                                    <li>Upp till 3 ytterligare sidor</li>
-                                    <li>Grundläggande designelement, t.ex. val av typsnitt och färgscheman</li>
-                                </ul>
-                                <span>1500-2500</span>
-                            </td>
-                            <td>
-                                <h4>Standard</h4>
-                                <ul>
-                                    <li>En anpassad hemsida</li>
-                                    <li>Upp till 5 ytterligare sidor</li>
-                                    <li>Mer avancerade designelement, t.ex. grafisk design, responsiv design och användarupplevelse</li>
-                                </ul>
-                                <span>2500-7500</span>
-                            </td>
-                            <td>
-                                <h4>Premium</h4>
-                                <ul>
-                                    <li>En anpassad hemsida</li>
-                                    <li>Upp till 10 sidor</li>
-                                    <li>Avancerade designelement, t.ex. animationer och multimedia</li>
-                                </ul>
-                                <span>7500-15000</span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><h3>Webbutveckling</h3></td>
-                            <td>
-                                <h4>Basic</h4>
-                                <ul>
-                                    <li>Grundläggande webbapplikation</li>
-                                    <li>Begränsad funktionalitet och funktioner</li>
-                                </ul>
-                                <span>2500-7500</span>
-                            </td>
-                            <td>
-                                <h4>Standard</h4>
-                                <ul>
-                                    <li>Komplex webbapplikation</li>
-                                    <li>Funktioner som användarautentisering och integration med databaser</li>
-                                </ul>
-                                <span>7500-15000</span>
-                            </td>
-                            <td>
-                                <h4>Premium</h4>
-                                <ul>
-                                    <li>Utveckling av en högst komplex webbapplikation</li>
-                                    <li>Avancerade funktioner som realtidsdataprocessering och integration med tredjeparts-API:er</li>
-                                </ul>
-                                <span>15000-25000</span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><h3>Apputveckling</h3></td>
-                            <td>
-                                <h4>Basic</h4>
-                                <ul>
-                                    <li>Grundläggande app</li>
-                                    <li>Begränsad funktionalitet och funktioner</li>
-                                </ul>
-                                <span>2500-7500</span>
-                            </td>
-                            <td>
-                                <h4>Standard</h4>
-                                <ul>
-                                    <li>Komplex app</li>
-                                    <li>Funktioner som användarautentisering och push-notifikationer</li>
-                                </ul>
-                                <span>7500-15000</span>
-                            </td>
-                            <td>
-                                <h4>Premium</h4>
-                                <ul>
-                                    <li>Utveckling av en högst komplex app</li>
-                                    <li>Avancerade funktioner som realtidsdataprocessering och integration med tredjeparts-API:er</li>
-                                </ul>
-                                <span>15000-25000</span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </motion.table>
+                <div className="hero-text">
+                    <div className="left">
+                        <h1>Prissättning</h1>
+                        <motion.h2 initial={{opacity: 0, translateX: "-100%"}} animate={{opacity: 1, translateX: 0}}>Våra prispaket på webbsidor</motion.h2>
+                    </div>
+                    <div className="right">
+                        <motion.p initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.2}}>Priserna nedan är för hemsidor med endast det angivna innehållet. Priset går upp vid extra tilägg.</motion.p>
+                        <motion.p initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.4}}>För en bättre uppskattning på pris, <RouterLink to='/contact' variant='link' arrow>Kontakta Oss</RouterLink></motion.p>
+                    </div>
+                </div>
+                <div className="packages">
+                    <motion.div initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.2}} className="package">
+                        <span className='tag'>Basic</span>
+                        <span className='from'>Från</span>
+                        <span className='price'>999 kr</span>
+                        <ul>
+                            <li>3 sidor</li>
+                            <li>Responsiv Design</li>
+                            <li>Kontaktformulär</li>
+                        </ul>
+                    </motion.div>
+                    <motion.div initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.4}} className="package">
+                        <span className='tag'>Standard</span>
+                        <span className='from'>Från</span>
+                        <span className='price'>2499 kr</span>
+                        <ul>
+                            <li>5 sidor</li>
+                            <li>Responsiv Design</li>
+                            <li>Kontaktformulär</li>
+                            <li>SEO</li>
+                            <li>UX</li>
+                        </ul>
+                    </motion.div>
+                    <motion.div initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.6}} className="package">
+                        <span className='tag'>Premium</span>
+                        <span className='from'>Från</span>
+                        <span className='price'>4999 kr</span>
+                        <ul>
+                            <li>8 sidor</li>
+                            <li>Responsiv Design</li>
+                            <li>Kontaktformulär</li>
+                            <li>SEO</li>
+                            <li>UX</li>
+                            <li>Blogg / Databas</li>
+                        </ul>
+                    </motion.div>
+                </div>
+                <div className="lower-text">
+                    <h2>Söker du även funktionalitet?</h2>
+                    <p>Vi erbjuder även utveckling av webbapplikationer och mobilapplikationer</p>
+                </div>
+                <div className="packages">
+                    <motion.div initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.2}} className="package">
+                        <span className='tag'>Basic</span>
+                        <span className='from'>Från</span>
+                        <span className='price'>1999 kr</span>
+                        <ul>
+                            <li>Enklare design</li>
+                            <li>Grundläggande funktioner</li>
+                        </ul>
+                    </motion.div>
+                    <motion.div initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.4}} className="package">
+                        <span className='tag'>Standard</span>
+                        <span className='from'>Från</span>
+                        <span className='price'>3999 kr</span>
+                        <ul>
+                            <li>Responsiv Design</li>
+                            <li>Användarautentisering</li>
+                            <li>Push-notifikationer</li>
+                            <li>Databaser</li>
+                        </ul>
+                    </motion.div>
+                    <motion.div initial={{opacity: 0, translateY: "100%"}} animate={{opacity: 1, translateY: 0}} transition={{delay: 0.6}} className="package">
+                        <span className='tag'>Premium</span>
+                        <span className='from'>Från</span>
+                        <span className='price'>7999 kr</span>
+                        <ul>
+                            <li>Responsiv Design</li>
+                            <li>Användarautentisering</li>
+                            <li>Push-notifikationer</li>
+                            <li>Databaser</li>
+                            <li>Realtidsdataprocessering</li>
+                            <li>Tredjeparts-API:er</li>
+                            <li>Webbshop</li>
+                        </ul>
+                    </motion.div>
+                </div>
             </div>
         </AnimatedPage>
     )
